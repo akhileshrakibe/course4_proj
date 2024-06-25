7eb2fb202f1024ce2995696a0e63bb50351783f7
Akhilesh Rakibe <akhileshrakibe@gmail.com>
2024-06-25 08:53:18 +0000

    Finish celery introduction

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -50,7 +50,8 @@ class Dev(Configuration):
       'django.contrib.messages',
       'django.contrib.staticfiles',
       'movies',
-      'gh'
+      'gh',
+      'django_celery_results'
   ]
 
   MIDDLEWARE = [
@@ -162,3 +163,5 @@ class Dev(Configuration):
   }
 
   OMDB_KEY = "1cfee322"
+  CELERY_RESULT_BACKEND = "django-db"
+  CELERY_BROKER_URL = "redis://localhost:6379/0"
